eb0986604e50aed277f3d3dabe50fd769808a2c6
elton-cs <govada.elton@gmail.com>
2024-01-11 19:29:49 -0600

    Add tests for refactor and circuit changes

--- contracts/src/recursive.test.ts
+++ contracts/src/recursive.test.ts
@@ -1,26 +1,32 @@
-import { Player, StartGameCircuit, StartGameCircuitProof, SubmitMoveCircuit, SubmitMoveCircuitProof } from './recursive';
+import { GameCircuitProof, GameCircuit } from './recursive';
 import { Bool, Field } from 'o1js';
+import { GameSet, MoveState, Player } from './types';
 
 describe('Start Game', () => {
     beforeAll(async () => {
-        await StartGameCircuit.compile();
-        await SubmitMoveCircuit.compile();
+        await GameCircuit.compile();
     })
 
-    let proof0: StartGameCircuitProof
-    let proof1: SubmitMoveCircuitProof
-    let proof2: SubmitMoveCircuitProof
+    let proof0: GameCircuitProof
+    let proof1: GameCircuitProof
+    let proof2: GameCircuitProof
 
-
-    it('create start game proof', async () => {
-        let playerStats = new Player({
+    it('create new game', async () => {
+        let setPlayerStats = new Player({
             health: Field(10),
             stamina: Field(0),
         })
-        console.log(playerStats)
+        let defaultMoveSet = new MoveState({
+            p1Move: Field(0),
+            p2Move: Field(0),
+        })
+        let setDefaultGameSet = new GameSet({
+            p1: setPlayerStats,
+            p2: setPlayerStats,
+            moveState: defaultMoveSet
+        })
         
-        proof0 = await StartGameCircuit.startNewGame(playerStats)
-
+        proof0 = await GameCircuit.startNewGame(setDefaultGameSet, setPlayerStats)
         let output = proof0.publicOutput
         console.log(output.moveState.p1Move.toJSON())
         console.log(output.moveState.p2Move.toJSON())
@@ -28,8 +34,16 @@ describe('Start Game', () => {
 
     it('player 1 submits a move',async () => {
         let plMove = Field(1)
-        proof1 = await SubmitMoveCircuit.submitMove(proof0, Bool(true), plMove)
+        proof1 = await GameCircuit.submitMove(proof0.publicOutput, proof0, Bool(true), plMove)
         let output = proof1.publicOutput
+        console.log(output.moveState.p1Move.toJSON())
+        console.log(output.moveState.p2Move.toJSON())
+    })
+
+    it('player 2 submits a move',async () => {
+        let p2Move = Field(3)
+        proof2 = await GameCircuit.submitMove(proof1.publicOutput, proof1, Bool(false), p2Move)
+        let output = proof2.publicOutput
         console.log(output.moveState.p1Move.toJSON())
         console.log(output.moveState.p2Move.toJSON())
     })
