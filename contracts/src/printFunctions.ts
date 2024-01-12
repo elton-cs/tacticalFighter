4b0cb68b95e743abfc666d0492f2dc1c40410f87
elton-cs <govada.elton@gmail.com>
2024-01-12 01:48:44 -0600

    Add print function to display move type

--- contracts/src/printFunctions.ts
+++ contracts/src/printFunctions.ts
@@ -4,14 +4,30 @@ import { GameSet, MoveState, Player } from "./types";
 export function printGameState(someGameState: GameSet) {
     let p1Health = someGameState.p1.health.toString() 
     let p1Stamina = someGameState.p1.stamina.toString()
-    let p1Move = someGameState.moveState.p1Move.toString()
+    let p1Move = printMove(someGameState.moveState.p1Move)
 
     let p2Health = someGameState.p2.health.toString() 
     let p2Stamina = someGameState.p2.stamina.toString()
-    let p2Move = someGameState.moveState.p2Move.toString()
+    let p2Move = printMove(someGameState.moveState.p2Move)
 
     console.log(`
     (Player 1) HP: ${p1Health} | SP: ${p1Stamina} || Move: ${p1Move} \n
     (Player 2) HP: ${p2Health} | SP: ${p2Stamina} || Move: ${p2Move} 
     `)
+}
+
+export function printMove(move: Field): String {
+    let string: String
+    if (move.toString() == "0") {
+        string = "none"
+    }
+    else if (move.toString() == "1") {
+        string = "charge"
+    }
+    else if (move.toString() == "2") {
+        string = "strike"
+    }
+    else string = "block"
+
+    return string
 }
